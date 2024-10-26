14ee3bac55717d6c7ebf0b1eb09f316f92a2c7c0
yuqiu2004 <2506852507@qq.com>
2024-10-26 22:46:57 +0800

    fix : bitXor

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -166,7 +166,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return (~x) & (~y);
+  return ~(~(~x&y) & ~(x&~y));
 }
 /* 
  * copyLSB - set all bits of result to least significant bit of x
